25c670cd8f421b7c83c383c503a8e59001f109c8
manuelalvarezsolt <manuel.alvarez.solt@gmail.com>
2026-04-07 11:31:48 -0300

    Update exercise_find_min.py

--- exercise_find_min.py
+++ exercise_find_min.py
@@ -12,3 +12,7 @@ def find_min(lista):
         El valor mínimo de la lista o None si está vacía
     """
     pass  # Reemplazar con tu implementación
+    if len(lista) == 0:
+        return(None)
+    else:
+        return min(lista)
